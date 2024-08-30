56124b139738a63df349604e2cc942ac8ef5790f
Jonas Ariel <jonasapm@gmail.com>
2024-08-30 16:31:33 -0300

    conclusão da atividade.

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -8,22 +8,43 @@ public class Consumidor {
     public static void main(String[] args) throws Exception {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("mqadmin");
+        connectionFactory.setPassword("Admin123XX_");
         Connection conexao = connectionFactory.newConnection();
         Channel canal = conexao.createChannel();
 
-        String NOME_FILA = "plica"
-                + "";
-        canal.queueDeclare(NOME_FILA, false, false, false, null);
+        String NOME_FILA = "task_queue";
+
+        boolean duravel = true;
+        int prefetchCount = 1;
+        canal.basicQos(prefetchCount);
+        canal.queueDeclare(NOME_FILA, duravel, false, false, null);
 
         DeliverCallback callback = (consumerTag, delivery) -> {
             String mensagem = new String(delivery.getBody());
             System.out.println("Eu " + consumerTag + " Recebi: " + mensagem);
+            
+            try {
+                doWork(mensagem);
+            } catch (Exception e) {
+                // TODO: handle exception
+            } finally {
+                System.out.println("Trabalho feito.");
+                canal.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
         };
-
+        boolean autoAck = false;
         // fila, noAck, callback, callback em caso de cancelamento (por exemplo, a fila foi deletada)
-        canal.basicConsume(NOME_FILA, true, callback, consumerTag -> {
+        canal.basicConsume(NOME_FILA, autoAck, callback, consumerTag -> {
             System.out.println("Cancelaram a fila: " + NOME_FILA);
         });
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char a : task.toCharArray()){
+            if (a == '.') Thread.sleep(1000);
+        }
     }
 }
 
--- src/main/java/Produtor.java
+++ src/main/java/Produtor.java
@@ -1,23 +1,43 @@
+import java.util.Random;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 public class Produtor {
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("mqadmin");
+        connectionFactory.setPassword("Admin123XX_");
         try (
                 Connection connection = connectionFactory.newConnection();
                 Channel canal = connection.createChannel();
         ) {
-            String mensagem = "Olá";
-            String NOME_FILA = "plica";
-
+            // String mensagem = "Olá";
+            // String mensagem = String.join("", args);
+            String NOME_FILA = "task_queue";
+            
             //(queue, passive, durable, exclusive, autoDelete, arguments)
-            canal.queueDeclare(NOME_FILA, false, false, false, null);
-
-            // ​(exchange, routingKey, mandatory, immediate, props, byte[] body)
-            canal.basicPublish("", NOME_FILA, false, false, null, mensagem.getBytes());
+            boolean duravel = true;
+            int prefetchCount = 1;
+            canal.basicQos(prefetchCount);
+            canal.queueDeclare(NOME_FILA, duravel, false, false, null);
+            
+            
+            for(int i=0; i< 10; i++){
+                String mensagem = "Mensagem com meu nome: Jonas Ariel Passos de Medeiros - " + i + " - ";
+                Random random = new Random();
+                for (int j = 0; j< random.nextInt(4) + 1; j++){
+                    mensagem += ".";
+                }
+                // ​(exchange, routingKey, mandatory, immediate, props, byte[] body)
+                canal.basicPublish("", NOME_FILA, false, false, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem.getBytes());
+                System.out.println("Mensagem publicada: " + mensagem);
+            }
 
         }
     }
